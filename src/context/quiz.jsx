5e9b472a6b4ad5e1615d9336f8929cf15fdd3eb7
guixaiver77 <xguilherme1@gmail.com>
2022-09-07 16:41:53 -0300

    ambiente-configurado

---
+++ src/context/quiz.jsx
@@ -0,0 +1,15 @@
+import { createContext , useReducer} from "react";
+import questions from '../data/questions';
+
+const STAGES = ['Start', 'Playing', 'End'];
+
+const initialState = {
+  gameStage: STAGES[0],
+}
+
+export const QuizContext = createContext();
+
+export const QuizProvider = ({children}) => {
+  const value = {name: 'Quiz'};
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
+};
